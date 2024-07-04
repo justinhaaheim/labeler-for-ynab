210fae2e5d9cb87299b202ff17cc75b408618b51
Justin Haaheim <justinhaaheim@users.noreply.github.com>
2024-07-04 05:14:57 -0700

    Fix "ReferenceError: require is not defined" error on build (#4)
    * Add engines
    
    * Upgrade vite
    
    * Try some other stuff
    
    * Fix require issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    // Added because of "ReferenceError: require is not defined" error coming from ynab dependency
+    // Source: https://stackoverflow.com/a/77543093/18265617
+    commonjsOptions: {transformMixedEsModules: true}, // Change
+  },
   plugins: [react()],
 });
