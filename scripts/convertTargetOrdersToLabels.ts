5336bae2c821d43203a8fc7d39c852435bc74ef2
Justin Haaheim <justinhaaheim@users.noreply.github.com>
2024-10-03 23:35:27 -0700

    Add tsignore to script

--- scripts/convertTargetOrdersToLabels.ts
+++ scripts/convertTargetOrdersToLabels.ts
@@ -1,3 +1,4 @@
+// @ts-ignore This file won't be present in all environments
 import fileToProcess from '../sample_data.local/2024-08-27__02-29-02__targetOrderData__2-of-2__invoiceAndOrderData__40-orders__includesAggregationsData.json';
 import {CombinedOutputDataZod} from '../src/TargetAPITypes';
 import {getLabelsFromTargetOrderData} from '../src/TargetConverters';
